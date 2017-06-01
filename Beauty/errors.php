96c6c441c118827044d3bc53677fcb107656f156
tigerk <kimhwawoon@gmail.com>
2017-06-01 11:59:24 +0800

    错误日志，记录堆栈信息

--- Beauty/errors.php
+++ Beauty/errors.php
@@ -2,8 +2,7 @@
 
 function handleException(Throwable $e)
 {
-    DLog::fatal("message:" . $e->getMessage() . " file=" . $e->getFile() . " line=" . $e->getLine(), 0, []);
+    DLog::fatal(var_export($e, true), 0, []);
 }
-
 
 set_exception_handler("handleException");
