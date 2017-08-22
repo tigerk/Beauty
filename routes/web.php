ff35c9ad82f50d9b879f5c1c24ff9a71521d998e
tigerk <kimhwawoon@gmail.com>
2017-08-22 18:46:02 +0800

    修改自动加载方式

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,27 @@
  * 最先设置的路由优先触发，未经验证
  */
 
+use Beauty\Core\App;
+
 /**
  * 菜谱搜索功能
  */
-\Beauty\Core\App::router()->get('/reg/[0-9]+/[0-9]+', 'TestController@test');
+App::router()->get('/reg/[0-9]+/[0-9]+', 'App\Controllers\TestController@test');
 
 /**
  * 添加过滤器，过滤器true，不添加到路由器解析中。
  */
-\Beauty\Core\App::router()->filter(function () {
+App::router()->filter(function () {
     return true;
 }, function () {
-    \Beauty\Core\App::router()->get('/', 'App\Controllers\TestController@test');
+    App::router()->get('/', 'App\Controllers\TestController@test');
 });
-
 
 /**
  * 添加过滤器，过滤器true，不添加到路由器解析中。
  */
-\Beauty\Core\App::router()->filter(function () {
+App::router()->filter(function () {
     return false;
 }, function () {
-    \Beauty\Core\App::router()->get('/hehe', 'TestController@test');
+    App::router()->get('/hehe', 'TestController@test');
 });
